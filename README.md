6b8c92e2da320468c3435af0c0c5447370769a3f
homsau <james.p.cowart@gmail.com>
2018-06-27 19:39:53 -0400

    commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Train-Scheduler
+
+<h1>When's your next train?!</h1>
+<p>Add the train name, the destination, the first train time, and the departure frequency.</p>
+<p>This app will tell you when the next arrival time is as well as how many minutes until the next train!</p>
+<h2>Don't miss your train!</h2>
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,8 +14,8 @@ $(document).ready(function() {
     // declare variables
     var database = firebase.database();
     var trainDB = database.ref("/trains");
-    var trainInfo = "";
     var presentTime = moment();
+    var trainInfo = "";
     var difference = "";
     var trainFrequency = "";
     var trainName = "";
@@ -26,8 +26,24 @@ $(document).ready(function() {
     var diffTimeConverted = "";
     var nextTrain = "";
     var nextTrainConverted = "";
-    var tRemainder = "";
+    var remainder = "";
 
+    // limit the amount of numbers able to be typed in the time area
+    $("input#first-train-time").on("keyup", function() {
+        limitText(this, 4);
+    });
+    function limitText(field, maxChar){
+        var ref = $(field);
+            val = ref.val();
+        if (val.length >= maxChar){
+            ref.val(function() {
+                //console.log(val.substr(0, maxChar))
+                return val.substr(0, maxChar);
+            });
+        }
+    }
+
+    // grab and add info to send to firebase.
     trainDB.on("child_added", function(childSnapshot){
         trainInfo = childSnapshot.val();
             trainName = trainInfo.trainName;
@@ -36,8 +52,8 @@ $(document).ready(function() {
             trainFrequency = trainInfo.trainFrequency;
             firstTrainConverted = moment(firstTrainTime, "HH:mm").subtract(1, "years");
             diffTime = moment().diff(moment(firstTrainConverted), "minutes");
-            tRemainder = diffTime % trainFrequency;
-            difference = trainFrequency - tRemainder;
+            remainder = diffTime % trainFrequency;
+            difference = trainFrequency - remainder;
             nextTrain = moment().add(difference, "minutes");
             nextTrainConverted = moment(nextTrain).format("HH:mm");
 
@@ -45,7 +61,8 @@ $(document).ready(function() {
             //console.log("next train: " + nextTrainConverted);
             //console.log("minutes till next: " + difference);
         
-        //nextTrain
+        // nextTrain
+        // adding the info from the "td" elements to the table
         var train = $("<tr>").addClass("train");
         train.append(
             $("<td>").addClass("trainName").text(trainName),
@@ -57,6 +74,7 @@ $(document).ready(function() {
         $("#trains-input").append(train);
     });
     
+    // take info from the input form and push it to firebase
     $("#add-train").click(function(event){
         event.preventDefault();
 
@@ -64,8 +82,13 @@ $(document).ready(function() {
         trainDestination = $("#train-destination").val().trim();
         firstTrainTime = $("#first-train-time").val().trim();
         trainFrequency = parseInt($("#departure-frequency").val().trim());
-    
-        if (firstTrainTime < 2400 && firstTrainTime >= 0) {
+        var first2Time = firstTrainTime.substring(0, 2);
+        var second2Time = firstTrainTime.substring(2, 4);
+        console.log(first2Time);
+        console.log(second2Time);
+
+        // make sure a number 0-2359 is added. not 2299 or 2510 but a valid military time
+        if (first2Time >= 0 && first2Time <= 23 && second2Time >= 0 && second2Time <= 59) {
             if (trainFrequency > 0) {
                 trainDB.push({
                     trainName,
@@ -73,8 +96,9 @@ $(document).ready(function() {
                     firstTrainTime,
                     trainFrequency
                 });
-                $("#first-train-time").val("");
-                $("#departure-frequency").val("");
+                // making sure the placeholder is correct if a valid answer is given
+                $("#first-train-time").val("").attr("placeholder", "HHmm");
+                $("#departure-frequency").val("").attr("placeholder", "60");
             } else {
                 $("#departure-frequency").val("").attr("placeholder", "Value must be greater than 0");
                 return;
@@ -83,7 +107,6 @@ $(document).ready(function() {
             $("#first-train-time").val("").attr("placeholder", "Enter a value between 0 and 2359");
             return;
         }
-        
         $("#train-name").val("");
         $("#train-destination").val("");
         $("#departure-frequency").val("");
